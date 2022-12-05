b52481b5cff6f41169786003cf2bcbca1f9637be
Paul Orfanidis <cs04134@uoi.gr>
2022-12-05 15:24:41 +0200

    added files for phpword class to be able to parse docx documents

--- php_finder/index.php
+++ php_finder/index.php
@@ -1,10 +1,28 @@
 <?php
+
+require __DIR__ .'/vendor/autoload.php';
+header('Content-Type: application/json; charset=utf-8');
+
+
+
 //do not show php warnings with wrong html syntax
 libxml_use_internal_errors(true);
-
 include_once("functions.php");
-echo get_current_menu();
 
+use PhpOffice\PhpWord\PhpWord;
+//echo get_current_menu();
 
+$doc_name = "./sitisis-dekemvrioy-2022.docx";
+$docx = new PhpWord($doc_name);
+//$docx = new \PhpOffice\PhpWord\CreateDocxFromTemplate($doc_name);
+$referenceNode = array(
+    'type' => 'table-cell',
+    'parent' => 'w:tbl/w:tr/',
+    'contains' => 'First row',
+);
+
+$contents = $docx->getWordStyles($referenceNode);
+
+echo contents;
 
 ?>
